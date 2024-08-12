bfa0b5a311c10aa5dc92625dc032fcecdcaf2247
DESKTOP-D2PFH23\yongjun <rladydwns248@gmail.com>
2024-08-12 22:20:44 +0900

    prject first

---
+++ performance/src/main/kotlin/com/performance/PerformanceApplication.kt
@@ -0,0 +1,11 @@
+package com.performance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PerformanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PerformanceApplication>(*args)
+}
